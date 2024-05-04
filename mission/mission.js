2cd3361ec2efdd056614f535c5f4e672da35a0d4
Darth-Volix <wil22159@byui.edu>
2024-05-04 10:32:49 -0600

    finished javascript for missions statement

--- mission/mission.js
+++ mission/mission.js
@@ -10,12 +10,16 @@ function applyTheme() {
         document.body.classList.add('dark'); // Add the .dark class to <body>
         const byuiLogo = document.getElementById('logo');
         const darkByuiLogo = document.createElement('img');
-        darkByuiLogo.src = 'mission/images/byui-logo_white.png';
-        darkByuiLogo.setAttribute('id', 'logo')
-    
-        byuiLogo.parentNode.replaceChild(darkByuiLogo, byuiLogo);
+        darkByuiLogo.src = 'images/byui-logo_white.png'; // Set the source for the white logo
+        darkByuiLogo.setAttribute('id', 'logo'); // Set the same ID as the existing logo
+        byuiLogo.parentNode.replaceChild(darkByuiLogo, byuiLogo); // Replace the existing logo with the white logo
     } else {
         document.body.classList.remove('dark'); // Remove .dark class
+        const byuiLogo = document.getElementById('logo');
+        const blueByuiLogo = document.createElement('img');
+        blueByuiLogo.src = 'images/byui-logo_blue.webp'; // Set the source for the blue logo
+        blueByuiLogo.setAttribute('id', 'logo'); // Set the same ID as the existing logo
+        byuiLogo.parentNode.replaceChild(blueByuiLogo, byuiLogo); // Replace the existing logo with the blue logo
     }
 }
 
@@ -24,4 +28,5 @@ themeSelect.addEventListener('change', applyTheme);
 
 // Apply initial theme based on user preference (if any)
 applyTheme();
+
 
