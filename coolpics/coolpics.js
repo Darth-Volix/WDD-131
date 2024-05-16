d858cac8aebe7fc653ed7a0ab7f7f84bfd5d3ab6
Darth-Volix <wil22159@byui.edu>
2024-05-16 12:02:25 -0600

    Refactor CSS and JavaScript for Cool Pics page

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -18,3 +18,43 @@ function handleResize() {
   
 handleResize();
 window.addEventListener("resize", handleResize);
+
+function viewerTemplate(pic, alt) {
+  return `<div class="viewer">
+    <button class="close-viewer">X</button>
+    <img class="viewer-img" src="${pic}" alt="${alt}">
+    </div>`;
+}
+
+function viewHandler(event) {
+  // Get the element that was clicked
+  const clickedElement = event.target;
+
+  // Get the src attribute from the clicked element
+  const srcAttribute = clickedElement.getAttribute('src');
+
+  // Split the src on the "-" character
+  const srcParts = srcAttribute.split('-');
+
+  // Construct the new image file name by adding "-full.jpeg" to the first part
+  const newFileName = srcParts[0] + '-full.jpeg';
+
+  // Create the viewer template using the new file name and alt text
+  const viewerHtml = viewerTemplate(newFileName, 'full size image');
+
+  // Insert the viewer template at the top of the body element
+  document.body.insertAdjacentHTML('afterbegin', viewerHtml);
+
+  // Add a listener to the close button (X)
+  const closeButton = document.querySelector('.close-viewer');
+  closeButton.addEventListener('click', closeViewer);
+}
+
+// Function to close the viewer
+function closeViewer() {
+  const viewer = document.querySelector('.viewer');
+  viewer.remove();
+}
+
+const gallerySection = document.querySelector('.gallery');
+gallerySection.addEventListener('click', viewHandler);
